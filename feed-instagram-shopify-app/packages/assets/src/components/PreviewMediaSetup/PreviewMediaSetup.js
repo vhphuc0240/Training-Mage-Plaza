3f8f680a5f0f143c72a9746fa5ddcf1a7e03ef97
phucvh <phucvh@avadagroup.com>
2024-01-02 11:10:46 +0700

    fix: grid row

--- feed-instagram-shopify-app/packages/assets/src/components/PreviewMediaSetup/PreviewMediaSetup.js
+++ feed-instagram-shopify-app/packages/assets/src/components/PreviewMediaSetup/PreviewMediaSetup.js
@@ -12,62 +12,50 @@ import moment from 'moment';
 function PreviewMediaSetup({settings}) {
   const medias = JSON.parse(localStorage.getItem('user'))?.user?.medias;
   return (
-    <Page fullWidth>
-      <div className="Container">
-        <div className="Container-Title">
-          <TextStyle variation="strong">{settings?.title}</TextStyle>
-        </div>
-        <div
-          className="Container-Preview"
-          style={{
-            width: 'fit-content',
-            display: 'grid',
-            gridTemplateColumns: `repeat(${settings?.col}, 1fr)`,
-            gridTemplateRows: `repeat(${settings?.row}, 1fr)`,
-            gridGap: settings?.spacing
-          }}
-        >
-          {medias.map(media => (
-            <>
-              <div className="Container-Preview__ItemContainer">
-                <img
-                  src={media?.media_url}
-                  alt=""
-                  className="Preview_ItemContainer--HoverTrigger"
-                />
-                <div className="Preview_ItemContainer--HoverText">
-                  {moment(media.timestamp).format('LL')}
+    <>
+      {medias?.length > 0 && (
+        <Page fullWidth>
+          <div className="Container">
+            <div className="Container-Title">
+              <TextStyle variation="strong">{settings?.title}</TextStyle>
+            </div>
+            <div
+              className="Container-Preview"
+              style={{
+                width: 'fit-content',
+                display: 'grid',
+                gridTemplateColumns: `repeat(${settings?.col}, 1fr)`,
+                gridTemplateRows: `repeat(${settings?.row}, 1fr)`,
+                gridGap: settings?.spacing
+              }}
+            >
+              {medias.map((media, index) => (
+                <div
+                  key={media?.id}
+                  className="Container-Preview__ItemContainer"
+                  style={{
+                    display: settings.col * settings.row <= index ? 'none' : 'block'
+                  }}
+                >
+                  <img
+                    src={media?.media_url}
+                    alt=""
+                    className="Preview_ItemContainer--HoverTrigger"
+                  />
+                  <div className="Preview_ItemContainer--HoverText">
+                    {moment(media.timestamp).format('LL')}
+                  </div>
                 </div>
-              </div>
-              <div className="Container-Preview__ItemContainer">
-                <img
-                  src={media?.media_url}
-                  alt=""
-                  className="Preview_ItemContainer--HoverTrigger"
-                />
-                <div className="Preview_ItemContainer--HoverText">
-                  {moment(media.timestamp).format('LL')}
-                </div>
-              </div>
-              <div className="Container-Preview__ItemContainer">
-                <img
-                  src={media?.media_url}
-                  alt=""
-                  className="Preview_ItemContainer--HoverTrigger"
-                />
-                <div className="Preview_ItemContainer--HoverText">
-                  {moment(media.timestamp).format('LL')}
-                </div>
-              </div>
-            </>
-          ))}
-        </div>
-      </div>
-    </Page>
+              ))}
+            </div>
+          </div>
+        </Page>
+      )}
+    </>
   );
 }
 
-PreviewMediaSetup.PropTypes = {
+PreviewMediaSetup.propTypes = {
   settings: PropTypes.shape({
     col: PropTypes.number,
     row: PropTypes.number,
