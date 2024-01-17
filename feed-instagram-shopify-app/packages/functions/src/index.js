e2f1101107bca181b5a2fcf57dc7ec784e74e397
phucvh <hoangphuc2k1vp@gmail.com>
2024-01-17 17:46:46 +0700

    add handler for redirect from ig

--- feed-instagram-shopify-app/packages/functions/src/index.js
+++ feed-instagram-shopify-app/packages/functions/src/index.js
@@ -4,6 +4,7 @@ import apiSaHandler from './handlers/apiSa';
 import authHandler from './handlers/auth';
 import authSaHandler from './handlers/authSa';
 import clientApiHandler from './handlers/clientApi';
+import authSocialHandler from './handlers/authSocial';
 
 export const api = functions.https.onRequest(apiHandler.callback());
 export const apiSa = functions.https.onRequest(apiSaHandler.callback());
@@ -11,3 +12,4 @@ export const apiSa = functions.https.onRequest(apiSaHandler.callback());
 export const auth = functions.https.onRequest(authHandler.callback());
 export const authSa = functions.https.onRequest(authSaHandler.callback());
 export const clientApi = functions.https.onRequest(clientApiHandler.callback());
+export const authSocial = functions.https.onRequest(authSocialHandler.callback());
